c7f8917ff4996d50fdd6b98706d3932d1593f5ed
vlzfr <l.dossantosvion@myskolae.fr>
2026-04-04 00:08:56 +0200

    fix du fix qui fix le fix

--- db.php
+++ db.php
@@ -14,7 +14,9 @@ $user   = getenv('DB_USER');
 $pass   = getenv('DB_PASS');
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8;sslmode=required", $user, $pass, [
+    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false
+]);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
