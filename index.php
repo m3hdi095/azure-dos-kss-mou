b2bf53866786d7d03e04286bfadc25d43ae09ad6
vlzfr <l.dossantosvion@myskolae.fr>
2026-04-04 00:16:26 +0200

    fix app.php

--- index.php
+++ index.php
@@ -7,12 +7,12 @@ $tasks = $pdo->query("SELECT * FROM tasks ORDER BY id DESC")->fetchAll(PDO::FETC
 <head>
     <meta charset="UTF-8">
     <title>Todo App</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <h1>📝 Todo List</h1>
 
-    <form action="add.php" method="POST">
+    <form action="app.php" method="POST">
         <input type="text" name="title" placeholder="Nouvelle tâche..." required>
         <button type="submit">Ajouter</button>
     </form>
